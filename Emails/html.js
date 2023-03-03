f88b35b33d86ca02d48b275848ae9fcbb3bd5e03
ccumaco <cumacopesca.5@nlsa.teleperformance.com>
2023-03-02 22:42:38 -0500

    url email producction

--- Emails/html.js
+++ Emails/html.js
@@ -1,9 +1,15 @@
 const successEmail = (resetToken, email) => {
+    let urlSite;
+    if (process.env.ENV == "dev") {
+        urlSite = "http://localhost:5173/"
+    } else {
+        urlSite = "https://incopy.netlify.app/"
+    }
     return `
         <p>Hola,</p>
         <p>Hemos recibido una solicitud para recuperar la contraseña de tu cuenta.</p>
         <p>Para continuar, haz clic en el siguiente enlace:</p>
-        <p><a href="http://localhost:5173/login?token=${resetToken}&email=${email}">Recuperar Contraseña</a></p>
+        <p><a href="${urlSite}login?token=${resetToken}&email=${email}">Recuperar Contraseña</a></p>
         <p>Si no solicitaste esta recuperación, por favor ignora este mensaje.</p>
     `
 }
