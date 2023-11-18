8563fe9f07a43dbc5b4b21432ca6abf1ab21773a
kiwicarlos <carlos@kiwicredito.com>
2023-11-17 22:03:19 -0500

    error browser

--- Services/puppeteer.js
+++ Services/puppeteer.js
@@ -46,11 +46,11 @@ const searchInGoogle = async ({ query }) => {
 
 
 const searchINMercadoLibre = async ({ query, justOne }) => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     try {
-        const browser = await puppeteer.launch({
-            headless: false,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
         const page = await browser.newPage();
         await page.goto('https://listado.mercadolibre.com.co/' + query);
         const pageData = await page.evaluate(() => {
@@ -75,39 +75,45 @@ const searchInEachProduct = async ({ urls, justOne }) => {
         headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
-
-    const data = [];
-    for (const url of urls) {
-        try {
-            const page = await browser.newPage();
-            await page.goto(url);
-
-            const pageData = await page.evaluate(() => {
-                const title = document.querySelector('h1.ui-pdp-title')?.innerText ?? 'No title founded';
-                const price = document.querySelector('.ui-pdp-price__second-line .andes-money-amount span.andes-money-amount__fraction')?.innerText ?? 'No price founded';
-                const features = [...document.querySelectorAll('.ui-vpp-highlighted-specs__features-list li')] ?? [' No features founded '];
-                const content = features?.map(p => {
-                    if (p && p.innerText) {
-                        return p.innerText;
-                    }
-                    return;
-                }) ?? [];
-                return {
-                    title,
-                    price,
-                    content
-                };
-            });
-            data.push({...pageData, url});
-        } catch (error) {
-            console.error(`Error processing URL: ${url.href}`, error.message);
-            return [];
+    try {
+        const data = [];
+        for (const url of urls) {
+            try {
+                const page = await browser.newPage();
+                await page.goto(url);
+    
+                const pageData = await page.evaluate(() => {
+                    const title = document.querySelector('h1.ui-pdp-title')?.innerText ?? 'No title founded';
+                    const price = document.querySelector('.ui-pdp-price__second-line .andes-money-amount span.andes-money-amount__fraction')?.innerText ?? 'No price founded';
+                    const features = [...document.querySelectorAll('.ui-vpp-highlighted-specs__features-list li')] ?? [' No features founded '];
+                    const content = features?.map(p => {
+                        if (p && p.innerText) {
+                            return p.innerText;
+                        }
+                        return;
+                    }) ?? [];
+                    return {
+                        title,
+                        price,
+                        content
+                    };
+                });
+                data.push({...pageData, url});
+            } catch (error) {
+                console.error(`Error processing URL: ${url.href}`, error.message);
+                return [];
+            }
         }
+        // await new Promise(resolve => setTimeout(resolve, 1000));
+        await browser.close();
+        if (!justOne) return data;
+        return data.slice(0, 1);
+    } catch (error) {
+        browser.close();
+        console.error(error);
+        return [];
     }
-    // await new Promise(resolve => setTimeout(resolve, 1000));
-    await browser.close();
-    if (!justOne) return data;
-    return data.slice(0, 1);
+
 }
 
 module.exports = {
